6da66902ad616dee8d66faa977a5a59a5bcf6f01
Doran Williams <116115557+Doran-28@users.noreply.github.com>
2024-04-03 15:24:32 -0400

    Add files via upload

--- GlFrame.java
+++ GlFrame.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -6,27 +8,35 @@ import javax.swing.JFrame;
 import com.jogamp.opengl.math.Vec3f;
 
 import solarModel.Planet;
+import solarModel.PlanetEnum;
 /**
  * A JFrame handler of the Jogl implementation's GLCanvas display
  */
-public class GlFrame extends JFrame{
+public class GlFrame extends JFrame implements KeyListener{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private Jogl jogl; //the current OpenGL implementation
+
+	private float savedRateOfTime;
+	// Used for pause and play feauture
+	private boolean isPlaying = true;
 	
 	/**
 	 * Creates the GLFrame with a window tile. 
 	 * The author chose to use "glf" as the standard naming convention
 	 * @param title The title (standard format: title.glf)
 	 */
-	public GlFrame(String title, float camX, float camY, float camZ, float focusX, float focusY, float focusZ) {
+	public GlFrame(String title) {
 		super(title);
-		jogl = new Jogl(camX, camY, camZ, focusX, focusY, focusZ);
-		this.add(jogl.getCanvas());
+		jogl = new Jogl();
 		this.setSize(jogl.getSize());
 		this.add(jogl.getCanvas()); //Adds the display canvas to the frame
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		this.setUndecorated(true);
+		
+		jogl.getCanvas().addKeyListener(this);
 		this.addWindowListener(new WindowAdapter() {
 	        public void windowClosing(WindowEvent e) {
 	            cut();
@@ -58,6 +68,87 @@ public class GlFrame extends JFrame{
 	public void loadArtbook(ArrayList<Planet> artbook) {
 		jogl.loadArtbook(artbook);
 	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_1:
+			jogl.setTrackedPlanet(PlanetEnum.MERCURY);
+			jogl.setRateOfTime(0.01f);
+			break;
+		
+		case KeyEvent.VK_2:
+			jogl.setTrackedPlanet(PlanetEnum.VENUS);
+			jogl.setRateOfTime(0.0045f);
+			break;
+			
+		case KeyEvent.VK_3:
+			jogl.setTrackedPlanet(PlanetEnum.EARTH);
+			jogl.setRateOfTime(0.5f);
+			break;
+			
+		case KeyEvent.VK_4:
+			jogl.setTrackedPlanet(PlanetEnum.MARS);
+			jogl.setRateOfTime(0.5f);
+			break;
+			
+		case KeyEvent.VK_5:
+			jogl.setTrackedPlanet(PlanetEnum.SATURN);
+			jogl.setRateOfTime(0.9f);
+			break;
+			
+		case KeyEvent.VK_6:
+			jogl.setTrackedPlanet(PlanetEnum.JUPITER);
+			jogl.setRateOfTime(1.1f);
+			break;
+			
+		case KeyEvent.VK_7:
+			jogl.setTrackedPlanet(PlanetEnum.URANUS);
+			jogl.setRateOfTime(1.0f);
+			break;
+			
+		case KeyEvent.VK_8:
+			jogl.setTrackedPlanet(PlanetEnum.NEPTUNE);
+			jogl.setRateOfTime(1.0f);
+			break;
+			
+		case KeyEvent.VK_0:
+			jogl.setTrackedPlanet(null);
+			jogl.setRateOfTime(100.0f);
+			break;
+			
+		case KeyEvent.VK_UP:
+			if (isPlaying)
+				jogl.increaseRateOfTime();
+			break;
+		
+		case KeyEvent.VK_DOWN:
+			if (isPlaying)
+				jogl.decreaseRateOfTime();
+			break;
 
-	
+
+		// Hitting the space bar will pause and play the animatuion
+		case KeyEvent.VK_SPACE:
+			if (isPlaying) {
+				savedRateOfTime = jogl.getRateOfTime();
+				jogl.setRateOfTime(0);
+				isPlaying = false;
+			}
+			else{
+				jogl.setRateOfTime(savedRateOfTime);
+				isPlaying = true;
+			}
+			break;
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}	
 }
